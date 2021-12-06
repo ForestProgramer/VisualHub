3a9481625958d9fa25ce631555a3c258afa2af2e
ForestProgramer <maksymoliiarnyk@nltu.lviv.ua>
2021-12-07 00:07:17 +0200

    Hey,i think this probably work better)

--- HubProject/Source.cpp
+++ HubProject/Source.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 string random(int len)
 {
+
 	string a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 	string r;
 	srand(time(NULL));
@@ -19,4 +20,27 @@ int main()
 	cout << random(32) << endl;
 	cin.get();
 	return 0;
+	int a[10][10], transpose[10][10], row, column, i, j;
+
+	cout << "Enter rows and columns of matrix: ";
+	cin >> row >> column;
+
+	cout << "\nEnter elements of matrix: " << endl;
+
+	// Storing matrix elements
+	for (int i = 0; i < row; ++i) {
+		for (int j = 0; j < column; ++j) {
+			cout << "Enter element a" << i + 1 << j + 1 << ": ";
+			cin >> a[i][j];
+		}
+	}
+
+	// Printing the a matrix
+	cout << "\nEntered Matrix: " << endl;
+	for (int i = 0; i < row; ++i) {
+		for (int j = 0; j < column; ++j) {
+			cout << " " << a[i][j];
+			if (j == column - 1)
+				cout << endl << endl;
+	
 }
